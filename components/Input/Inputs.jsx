a9dafe719fb2741d5c11ca456ff9f34f569b48a7
PedroSpelta <p.h.s.8989@hotmail.com>
2021-11-23 01:09:07 -0300

    adicionando o input sem stilo

--- components/Input/Inputs.jsx
+++ components/Input/Inputs.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Listbox } from "@headlessui/react";
+
+const types = [
+  { id: 1, name: "Grass" },
+  { id: 2, name: "Poison" },
+  { id: 3, name: "Fire" },
+  { id: 4, name: "Flying" },
+  { id: 5, name: "Water" },
+  { id: 6, name: "Bug" },
+  { id: 7, name: "Normal" },
+  { id: 8, name: "All" },
+];
 
 function Inputs() {
+  const [selected, setSelected] = useState(types[0]);
+
   return (
-    <div className="flex items-center justify-center gap-2">
-      teste
-      haha
-      eae
-    </div>
-  )
+    <Listbox value={selected} onChange={setSelected}>
+      <Listbox.Button>{selected.name}</Listbox.Button>
+      <Listbox.Options>
+        {types.map((type) => (
+          <Listbox.Option key={type.id} value={type} disabled={false}>
+            {type.name}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
+    </Listbox>
+  );
 }
 
-export default Inputs
+export default Inputs;
